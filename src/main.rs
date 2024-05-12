7465571cd61d53c06b74bb8a4904b8a0da064e79
Akarvin <akarvin2912@gmail.com>
2024-05-12 03:54:35 -0400

    multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
 use std::io::*;
+use std::thread;
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -12,10 +14,17 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                loop {
-                    let _ =  stream.read(&mut [0; 1024]);
-                    let _ = stream.write(b"+PONG\r\n");
-                }
+                thread::spawn(move || {
+                    let mut buffer = [0; 1024];
+                    loop {
+                        let _ = stream.read(&mut buffer);
+                        let _ = stream.write(b"+PONG\r\n");
+                    }
+                });
+                // loop {
+                //     let _ =  stream.read(&mut [0; 1024]);
+                //     let _ = stream.write(b"+PONG\r\n");
+                // }
                 // let _ = stream.write(b"+PONG\r\n");
                 // stream.write(b"+PONG\r\n");
    
