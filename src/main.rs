ae3f5ec2d3321f2c5fe78aac60886efbfaad08da
Akarvin <akarvin2912@gmail.com>
2024-05-12 00:48:28 -0400

    uncommented

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
